ac3f86c857bf7a0fc5b059f7bf4341e461e51d5a
merlin <clamores.pro@gmail.com>
2025-06-08 13:40:39 +0300

    Implement Open-Meteo air quality client and view template

---
+++ cmd/openmeteo/openmeteo.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+	"unicode"
+
+	"github.com/ninedraft/daily-bacon/internal/client"
+	"github.com/ninedraft/daily-bacon/internal/meteo"
+	"github.com/ninedraft/daily-bacon/internal/view"
+)
+
+func main() {
+	exitCode := 0
+	defer func() {
+		if exitCode != 0 {
+			_ = os.Stdout.Sync()
+			os.Exit(exitCode)
+		}
+	}()
+
+	log.SetFlags(log.Lshortfile)
+
+	params := meteo.Params{
+		Longitude:    0,
+		Latitude:     0,
+		PastDays:     1,
+		ForecastDays: 1,
+		Current:      []string{"pm10", "pm2.5", "dust", "european_aqi"},
+		StartDate:    time.Now().AddDate(0, 0, -1),
+		EndDate:      time.Now().AddDate(0, 0, 1),
+		Daily:        []string{},
+		Timezone:     time.Local.String(),
+	}
+	bindRequestFlags(flag.CommandLine, "api", &params)
+
+	timeout := 10 * time.Second
+	flag.DurationVar(&timeout, "client.timeout", timeout, "HTTP client timeout")
+
+	flag.Parse()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	cl := client.New(http.DefaultClient.Transport)
+	meteoClient := meteo.New(cl)
+
+	resp, err := meteoClient.AirQuality(ctx, params)
+	if err != nil {
+		log.Printf("doing request: %v", err)
+		exitCode = 10
+		return
+	}
+
+	if err := view.AirQuality(os.Stdout, resp); err != nil {
+		log.Printf("formatting response: %v", err)
+		exitCode = 11
+	}
+}
+
+func bindRequestFlags(flags *flag.FlagSet, prefix string, p *meteo.Params) {
+	if prefix != "" {
+		prefix += "."
+	}
+
+	flags.Float64Var(&p.Latitude, prefix+"latitude", p.Latitude, "latitude for air quality request")
+	flags.Float64Var(&p.Longitude, prefix+"longitude", p.Longitude, "longitude for air quality request")
+
+	flags.Func(prefix+"current", "current variables", func(value string) error {
+		fields := strings.FieldsFuncSeq(value, flagSliceField)
+		for field := range fields {
+			p.Current = append(p.Current, field)
+		}
+		return nil
+	})
+
+	flags.Func(prefix+"hourly", "hourly variables", func(value string) error {
+		fields := strings.FieldsFuncSeq(value, flagSliceField)
+		for field := range fields {
+			p.Hourly = append(p.Hourly, field)
+		}
+		return nil
+	})
+
+	flags.Func(prefix+"daily", "daily variables", func(value string) error {
+		fields := strings.FieldsFuncSeq(value, flagSliceField)
+		for field := range fields {
+			p.Daily = append(p.Daily, field)
+		}
+		return nil
+	})
+
+	flags.Func(prefix+"start-date", "start date (YYYY-MM-DD)", func(value string) error {
+		t, err := time.Parse(time.DateOnly, value)
+		if err != nil {
+			return fmt.Errorf("%q: time.Parse: %w", value, err)
+		}
+		p.StartDate = t
+		return nil
+	})
+
+	flags.Func(prefix+"end-date", "end date (YYYY-MM-DD)", func(value string) error {
+		t, err := time.Parse(time.DateOnly, value)
+		if err != nil {
+			return fmt.Errorf("%q: time.Parse: %w", value, err)
+		}
+		p.EndDate = t
+		return nil
+	})
+
+	flags.StringVar(&p.Timezone, prefix+"timezone", p.Timezone, "timezone for dates returned")
+	flags.IntVar(&p.ForecastDays, prefix+"forecast-days", p.ForecastDays, "number of forecast days")
+	flags.IntVar(&p.PastDays, prefix+"past-days", p.PastDays, "number of past days")
+}
+
+func flagSliceField(ru rune) bool {
+	return strings.ContainsRune(",|", ru) || unicode.IsSpace(ru)
+}
